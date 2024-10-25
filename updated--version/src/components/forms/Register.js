0f0991c7894309b5506e9e329f5267ae6e8f8f77
haile <haileleulfiseha@gmail.com>
2024-10-25 14:17:57 +0300

    edited the api

--- updated--version/src/components/forms/Register.js
+++ updated--version/src/components/forms/Register.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { auth, createUserWithEmailAndPassword, updateProfile } from '../../config/Fire';
-// import './Reg.css';
-import './Login.css'
+import './Login.css';
 
 class Register extends Component {
     state = {
--- updated--version/src/config/Fire.js
+++ updated--version/src/config/Fire.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCQOH-6VpubSS1IAyusSWDsRDtlRb9jpgo",
-	authDomain: "expense-tracker-aed26.firebaseapp.com",
-	projectId: "expense-tracker-aed26",
-	storageBucket: "expense-tracker-aed26.appspot.com",
-	messagingSenderId: "835295357125",
-	appId: "1:835295357125:web:d261d7221d0ca8c292a8ac",
-	measurementId: "G-YSTJMDWX7X",
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
