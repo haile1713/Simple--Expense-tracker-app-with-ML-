e285a9db40d8fbf9c343d34727d160a6b867d65e
haile <haileleulfiseha@gmail.com>
2024-11-02 14:40:47 +0300

    enhanced user friendly ui

--- updated--version/src/App.js
+++ updated--version/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Main from "./components/main";
 function App() {
-	return <Main />;
+	return (
+		<div>
+			<h1>Hello!</h1>
+			<h2>Manage your finances effortlessly! 💸 </h2>
+			<Main />;
+		</div>
+	);
 }
 
 export default App;
